f7378b6d1dde37ed9cfedf10943a5a74cfabe785
Dan Rotondo <dm30roto@gmail.com>
2012-09-29 18:05:56 -0400

    Making the new design live

--- js/app.js
+++ js/app.js
@@ -1,109 +1,14 @@
-/* Foundation v2.2.1 http://foundation.zurb.com */
-jQuery(document).ready(function ($) {
-	/* Use this js doc for all application specific JS */
-
-	/* TABS --------------------------------- */
-	/* Remove if you don't need :) */
-
-	function activateTab($tab) {
-		var $activeTab = $tab.closest('dl').find('a.active'),
-				contentLocation = $tab.attr("href") + 'Tab';
-				
-		// Strip off the current url that IE adds
-		contentLocation = contentLocation.replace(/^.+#/, '#');
-
-		//Make Tab Active
-		$activeTab.removeClass('active');
-		$tab.addClass('active');
-
-    //Show Tab Content
-		$(contentLocation).closest('.tabs-content').children('li').hide();
-		$(contentLocation).css('display', 'block');
-	}
-
-	$('dl.tabs').each(function () {
-		//Get all tabs
-		var tabs = $(this).children('dd').children('a');
-		tabs.click(function (e) {
-			activateTab($(this));
-		});
-	});
-
-	if (window.location.hash) {
-		activateTab($('a[href="' + window.location.hash + '"]'));
-		$.foundation.customForms.appendCustomMarkup();
-	}
-
-	/* ALERT BOXES ------------ */
-	$(".alert-box").delegate("a.close", "click", function(event) {
-    event.preventDefault();
-	  $(this).closest(".alert-box").fadeOut(function(event){
-	    $(this).remove();
-	  });
-	});
-
-
-	/* PLACEHOLDER FOR FORMS ------------- */
-	/* Remove this and jquery.placeholder.min.js if you don't need :) */
-
-	$('input, textarea').placeholder();
-
-	/* TOOLTIPS ------------ */
-	$(this).tooltips();
-
-
-
-	/* UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE6/7/8 SUPPORT AND ARE USING .block-grids */
-//	$('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'left'});
-//	$('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'left'});
-//	$('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'left'});
-//	$('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'left'});
-
-
-
-	/* DROPDOWN NAV ------------- */
-
-	var lockNavBar = false;
-	$('.nav-bar a.flyout-toggle').live('click', function(e) {
-		e.preventDefault();
-		var flyout = $(this).siblings('.flyout');
-		if (lockNavBar === false) {
-			$('.nav-bar .flyout').not(flyout).slideUp(500);
-			flyout.slideToggle(500, function(){
-				lockNavBar = false;
-			});
-		}
-		lockNavBar = true;
-	});
-  if (Modernizr.touch) {
-    $('.nav-bar>li.has-flyout>a.main').css({
-      'padding-right' : '75px'
-    });
-    $('.nav-bar>li.has-flyout>a.flyout-toggle').css({
-      'border-left' : '1px dashed #eee'
-    });
-  } else {
-    $('.nav-bar>li.has-flyout').hover(function() {
-      $(this).children('.flyout').show();
-    }, function() {
-      $(this).children('.flyout').hide();
-    })
-  }
-
-
-	/* DISABLED BUTTONS ------------- */
-	/* Gives elements with a class of 'disabled' a return: false; */
-	
-	$('.logo a').click(function(){
+jQuery(document).ready(function ($) {	
+	$('a.brand').click(function(){
 		var el = $(this).attr('href');
 		var elWrapped = $(el);
-		scrollToDiv(elWrapped,180);
+		scrollToDiv(elWrapped,50);
 		return false;
 	});
-	$('nav ul li a').click(function(){
+	$('ul.main-menu li a').click(function(){
 		var el = $(this).attr('href');
 		var elWrapped = $(el);
-		scrollToDiv(elWrapped,180);
+		scrollToDiv(elWrapped,50);
 		return false;
 	});
 	function scrollToDiv(element,navheight){
@@ -116,5 +21,39 @@ jQuery(document).ready(function ($) {
 		}, 500);
 	
 	}  
-	$(".logo a").lettering().animateLetters({opacity:0},{opacity:1},{time:1200,reset:true});
+	//$("a.brand").lettering().animateLetters({opacity:0},{opacity:1},{time:1200,reset:true});
+	var email=$('.email'), subj=$('.subject'), msg=$('.message'), submit=$('.btn-submit');
+	function checkInputs(){
+	  if(email.val() != '' && subj.val() != '' && msg.val() != '' && submit.attr("disabled") == "disabled"){
+		submit.removeAttr("disabled");
+	  }
+	  else if((email.val() == '' || subj.val() == '' || msg.val() == '') && submit.attr("disabled") == undefined){
+		submit.attr("disabled", "disabled");
+	  }
+	}
+	email.keyup(function () {
+	  checkInputs();
+	});
+	subj.keyup(function () {
+	  checkInputs();
+	});
+	msg.keyup(function () {
+	  checkInputs();
+	});
+	
+	$("#emailForm").submit(function(event) {
+		/* stop form from submitting normally */
+		event.preventDefault(); 
+		// Make a request to backend
+		$.post('email.php', { emailAddr: email.val(), subject: subj.val(), message: msg.val() }, 
+			function( data ) {
+			  if(data) {
+				  alert('Email Success! :)');
+			  }
+			  else {
+				  alert('Email Error! Please provide a valid email address.');  
+			  }
+		  });
+		  return false;
+	});
 });
