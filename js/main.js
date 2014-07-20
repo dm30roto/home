0e26847996d650917c68aa017d896c32de8ea328
Dan Rotondo <dm30roto@gmail.com>
2014-07-20 15:06:08 -0700

    new home

--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
 /* Foundation v2.2.1 http://foundation.zurb.com */
 jQuery(document).ready(function ($) {
-	$(".jumbotron h1").lettering().animateLetters({opacity:0},{opacity:1},{randomOrder:false,time:1200,reset:true});
-	$(".jumbotron p").lettering().animateLetters({opacity:0},{opacity:1},{randomOrder:false,time:500,reset:true});
+	// $(".jumbotron h1").lettering().animateLetters({opacity:0},{opacity:1},{randomOrder:false,time:1200,reset:true});
+	// $(".jumbotron p").lettering().animateLetters({opacity:0},{opacity:1},{randomOrder:false,time:500,reset:true});
 	
+	$('#home').animate({'opacity':'1'},600);    
+	$('#about').animate({'opacity':'1'},1000);    
+
+	/* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.hideme').each( function(i) {
+            
+            var bottom_of_object = $(this).position().top - 50;// + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object )
+            {
+                $(this).animate({'opacity':'1'},1000);        
+            }
+        }); 
+    });
+
 	$('a.navbar-brand').click(function(){
 		var el = $(this).attr('href');
 		var elWrapped = $(el);
